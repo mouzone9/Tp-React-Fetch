4f6873630db281a1036d42570bb7f65035661684
Jessy <ernatusjessy@gmail.com>
2022-05-31 14:31:51 +0200

    add components formular

--- Frontend/front-end-tp-react/src/components/formular.js
+++ Frontend/front-end-tp-react/src/components/formular.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React       from 'react'
+import { useForm } from "react-hook-form";
+import '../styles/formular.css'
 
 function Formular() {
-  return <>
-    <h1>The Formular</h1>
-  </>
+    const { handleSubmit, register } = useForm();
+    const onSubmit = data => console.log(data);
+
+    return <>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="container">
+                <div className="menu">
+                    <a className="" href='#'>
+                        <h2>Se connecter</h2>
+                    </a>
+                    <div className='connexion'>
+                        <div className='contact-form'>
+                            <label>USERNAME</label>
+                            <input 
+                                placeholder='username'
+                                {...register("username")}
+                            />
+                            <label>PASSWORD</label>
+                            <input
+                                placeholder='password'
+                                {...register("password")}
+                            />
+                            <div className='spaceBetweenTerms' />
+                            <input className='submit' value="connect" type='submit' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </>
 }
 
 export default Formular;
