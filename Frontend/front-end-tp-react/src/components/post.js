b8f4679227dabff669f1ceba20f2abed378c6e2b
Jessy <ernatusjessy@gmail.com>
2022-06-01 23:18:11 +0200

    add post data submit for api

--- Frontend/front-end-tp-react/src/components/post.js
+++ Frontend/front-end-tp-react/src/components/post.js
@@ -1,19 +1,45 @@
 import React from 'react'
+import { useState } from 'react'
 import '../styles/post.css'
 
 function Post() {
-    return (
-        <div className='callout secondary'>
-            <h4 className='leave-comment'>
-                Add a post
-            </h4>
-            <form className='post-edit'>
-                <input placeholder='AUTHOR' type='text'/>
-                <textarea placeholder='add your comments'></textarea>
-                <button>Add comment</button>
-            </form>
+    
+    const[data, setData] = useState({
+        author: "",
+        content: ""
+    })
+    
+    function onChange(event) {
+        event.preventDefault();
+        const newData = {...data}
+        newData[event.target.id] = event.target.value
+        setData(newData)
+    }
+    
+    function submit(event) {
+        event.preventDefault();
+        console.log(data)
+    }
+
+    return <>
+        <div className='container-post post'>
+            <div className='callout secondary'>
+                <h4 className='leave-comment'>
+                    Add a post
+                </h4>
+                <form onSubmit={(event)=> submit(event)} className='post-edit'>
+                    <label>Author</label>
+                    <input onChange={(event) => onChange(event)} id="author" value={data.author} placeholder='AUTHOR' type='text'/>
+                    <label>Content</label>
+                    <input onChange={(event) => onChange(event)} id="content" value={data.content} placeholder='CONTENT' type='text'/>
+                    <button>Add comment</button>
+                </form>
+                <div>
+                    {/*{this.renderPost()}*/}
+                </div>
+            </div>
         </div>
-    )
+    </>
 }
 
 export default Post;
